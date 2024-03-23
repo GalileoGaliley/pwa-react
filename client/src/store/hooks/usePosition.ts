83a694afdbb765d13d6296fcd512a3ddd61b1254
Tokan Vlad <vlad.tokan96@mail.ru>
2024-03-23 20:05:41 +0300

    ADD Map

--- client/src/store/hooks/usePosition.ts
+++ client/src/store/hooks/usePosition.ts
@@ -1,15 +1,33 @@
 import { useState, useEffect } from 'react';
 
+export interface PosData {lat: number, lon: number }
 export const usePosition = () => {
-  const [position, setPosition] = useState<{lat: number, lon: number } | null>(null);
+  const [position, setPosition] = useState<PosData | null>(null);
   const [error, setError] = useState('');
 
   useEffect(() => {
-    navigator.geolocation.getCurrentPosition(({coords: {latitude, longitude}}) => {
-      setPosition({lat: latitude, lon: longitude})
-    }, ({message}) => {
-      setError(message)
-    });
+    const geo = navigator.geolocation;
+
+    if (!geo) {
+      setError('Геолокация не поддерживается браузером');
+      return;
+    }
+    const onChange = ({lat, lon}: PosData) => {
+      setPosition({lat, lon});
+    };
+    const onError = (err: string) => {
+      setError(err);
+    };
+    const watcher =
+      geo.watchPosition((data) =>
+        onChange({
+          lat: data.coords.latitude,
+          lon: data.coords.longitude
+        }),
+        (err) => onError(err.message)
+      );
+
+    return () => geo.clearWatch(watcher);
   }, []);
 
   return {data: position, error: error };
